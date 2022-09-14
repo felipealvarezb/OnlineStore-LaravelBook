467278ab0d4a87a7aec5d9f8b18c6a689ddf03bd
felipealvarezb <felipealvarezb1@gmail.com>
2022-09-14 17:35:27 -0500

    Upload all code book

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', 'Home Page - Online Store') 
+@section('subtitle', 'Este es el subtitulo')
+@section('footer', 'Este es el footer')
+@section('content')
+    <div class="text-center">
+        Welcome to the application 
+    </div>
+@endsection
